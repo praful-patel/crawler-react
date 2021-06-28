b043ad53fddfb691527f4b8fd5ac99d89d26a5d6
Praful Patel <praful@techalchemy.co>
2021-06-28 21:56:06 +0530

    New modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Table } from "reactstrap";
+import { Container, Table, Button, Col, Row, Spinner } from "reactstrap";
 import axios, { AxiosResponse } from "axios";
 
 interface ServerResponse {
@@ -24,54 +24,94 @@ const initialCrawlData: Array<Crawl> = [
 
 const App = () => {
   const [crawlData, setCrawlData] = useState<Array<Crawl>>(initialCrawlData);
+  const [tableShow, setTableShow] = useState(false);
+  const [click, setClick] = useState(false);
 
   // API call
   useEffect(() => {
+    if (tableShow === true) {
+      const intervalId = setInterval(() => {
+        axios
+          .get("http://localhost:8021/crawl")
+          .then((response: AxiosResponse<ServerResponse>) => {
+            console.log("res", response);
+            setCrawlData(response.data.data);
+          });
+      }, 5000);
+      return () => clearInterval(intervalId);
+    }
+  }, [tableShow]);
+
+  const handleClick = () => {
+    setClick(true);
     axios
-      .get("http://localhost:8021/crawl")
+      .post("http://localhost:8021/crawl")
       .then((response: AxiosResponse<ServerResponse>) => {
-        console.log("res", response);
-        setCrawlData(response.data.data);
+        console.log("res", response.data.data);
+        axios
+          .get("http://localhost:8021/crawl")
+          .then((response: AxiosResponse<ServerResponse>) => {
+            console.log("res", response);
+            setCrawlData(response.data.data);
+            setClick(false);
+            setTableShow(true);
+          });
       });
-  }, []);
+  };
 
   return (
     <Container>
       <div>
-        <Table responsive bordered>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Page Url</th>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Headings</th>
-            </tr>
-          </thead>
-          <tbody>
-            {crawlData.length > 1 ? (
-              crawlData.map((data, i) => (
-                <tr>
-                  <th scope="row">{i + 1}</th>
-                  <td>
-                    <a href={data.pageUrl}>{data.pageUrl}</a>
-                  </td>
-                  <td>{data.pageTitle}</td>
-                  <td>{data.pageDescription}</td>
-                  <td>
-                    <ul>
-                      {data.pageHeadings.map((heading) => (
-                        <li>{heading}</li>
-                      ))}
-                    </ul>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>No data available</tr>
-            )}
-          </tbody>
-        </Table>
+        {tableShow === false ? (
+          <Row>
+            <Col></Col>
+            <Col>
+              {click === true ? (
+                "Loading...."
+              ) : (
+                <Button onClick={() => handleClick()} className="mt-5">
+                  Start Crawling
+                </Button>
+              )}
+            </Col>
+            <Col></Col>
+          </Row>
+        ) : (
+          <Table responsive bordered>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Page Url</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Headings</th>
+              </tr>
+            </thead>
+            <tbody>
+              {crawlData.length > 0 ? (
+                crawlData.map((data, i) => (
+                  <tr>
+                    <th scope="row">{i + 1}</th>
+                    <td>
+                      <a href={data.pageUrl}>{data.pageUrl}</a>
+                    </td>
+                    <td>{data.pageTitle}</td>
+                    <td>{data.pageDescription}</td>
+                    <td>
+                      <ul>
+                        {data.pageHeadings.map((heading) => (
+                          <li>{heading}</li>
+                        ))}
+                      </ul>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>No data available</tr>
+              )}
+            </tbody>
+          </Table>
+        )}
       </div>
     </Container>
   );
